5bf786d697d08fc9ab2e3e91049343d371f9dc92
Alexander Dominguez <alexander.dominguez@jayacompany.com>
2019-09-11 20:39:58 -0500

    playing around with db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const server = express();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'alex',
+  // database: 'gsn_dev'
+});
+
+connection.query(`
+  CREATE DATABASE newtest;
+`, (err, result) => {
+  if (err) throw err;
+  console.log({result});
+});
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('connected');
+// });
 
 server.listen(3000, () => {
   console.log('boiler plate server listening');
